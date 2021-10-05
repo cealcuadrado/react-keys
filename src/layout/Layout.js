97d4beda48650d188ca7620d771b19513045a38d
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-05 18:27:23 -0300

    Creados datos en el layout, y condiciones para mostrar datos en el componente Content

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,12 +2,22 @@ import React from "react";
 import Content from "../main/Content";
 
 class Layout extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            data: [
+                { id: 1, component: 'First' },
+                { id: 2, component: 'Second' },
+                { id: 3, component: 'Third'}
+            ]
+        };
+    }
     render() {
         return (
             <div>
-                <p>Hola, mundo!</p>
-                <hr/>
-                <Content/>
+                {this.state.data.map((dynamicComponent, i) => 
+                    <Content key={i} componentData={dynamicComponent}/>)}
             </div>
         );
     }
--- src/main/Content.js
+++ src/main/Content.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Content extends React.Component {
     render() {
         return (
-            <p>Content</p>
+            <div>
+                <p>ID {this.props.componentData.id}: {this.props.componentData.component}</p>
+            </div>
         );
     }
 }
